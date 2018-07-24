163ab3cdb6826faef2af6615e03acf0f15430d63
aditya98617 <aditya98617@LAPTOP-TUB69LHR>
2018-07-25 01:04:16 +0530

    changes

--- FileIt-Services/src/main/java/com/kirat/solutions/service/BinderService.java
+++ FileIt-Services/src/main/java/com/kirat/solutions/service/BinderService.java
@@ -300,7 +300,7 @@ public class BinderService {
 	@POST
 	@Path("classifiedData")
 	public JSONObject getBookClassification() throws Exception {
-		if (FileItContext.get(BinderConstants.CLASSIFIED_BOOK_NAMES) == null || !FileUtil.checkBookClassificationJson()) {
+		if (FileItContext.get(BinderConstants.CLASSIFIED_BOOK_NAMES) == null) {
 			PrepareClassificationMap
 					.createClassifiedMap(FileInfoPropertyReader.getInstance().getString("masterjson.file.path"));
 		}
